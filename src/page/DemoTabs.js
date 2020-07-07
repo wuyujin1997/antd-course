5db3c23a82549e59697fab947b1aacec2ecfc604
yjwu-win10 <yjwu.bach@outlook.com>
2020-07-07 22:57:40 +0800

    Tabs组件 受控 activeKey属性测试

--- src/page/DemoTabs.js
+++ src/page/DemoTabs.js
@@ -15,17 +15,40 @@ const {
 
 export default class DemoTabs extends React.Component {
 
+    state = {
+        activeKey: "3",
+    }
+
+    // 使用 ES6 的箭头函数，会自动绑定其中的 this 的指向。
+    onTabChange = (activeKey) => {
+        console.log("点击之前激活的TabPane: %s\n点击之后激活的TabPane: %s", this.state.activeKey, activeKey);
+        this.setState({
+            activeKey: activeKey,
+        });
+    }
+    
     render() {
 
+        // 非受控：
         // Tabs组件中的多个TabPane，默认激活哪个？第一个。
+        // var element = (
+        //     <Tabs>
+        //         <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
+        //         <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+        //     </Tabs>
+        // );
+
+        // 受控：
+        // 通过设置 Tabs 标签的 activeKey 属性，可以从 Tabs组件外部决定：Tabs中激活第几个TabPane。
         var element = (
-            <Tabs>
+            <Tabs activeKey={this.state.activeKey} onChange={this.onTabChange}>
                 <TabPane tab="Tab 1" key="1">Content of Tab Pane 1</TabPane>
                 <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+                <TabPane tab="Tab 3" key="3">Content of Tab Pane 3</TabPane>
             </Tabs>
         );
         
-        return element
+        return element;
     }
     
 }
