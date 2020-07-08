ec8a1f280d5e6517f2eade047995ff6c7deb063c
yjwu-win10 <yjwu.bach@outlook.com>
2020-07-08 23:01:17 +0800

    A 添加卡片按钮及功能(添加自动生成的数据)

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {
     Card,
+    Button,
 } from 'antd';
 
 export default class PuzzleCardsPage extends React.Component {
@@ -9,6 +10,7 @@ export default class PuzzleCardsPage extends React.Component {
     // 构造方法
     constructor(props) {
         super(props);
+        this.counter = 100;
         // 初始化状态 state
         this.state  = {
             cardList: [
@@ -26,6 +28,30 @@ export default class PuzzleCardsPage extends React.Component {
         };
     }
 
+    addNewCard = () => {
+        var callback = (prevState) => {
+            // 保存之前的卡片列表(的数据)
+            const prevCardList = prevState.cardList;
+
+            // 新的card对象
+            this.counter += 1;
+            const card = {
+                id: this.counter,
+                setup: `test setup ${this.counter}`,
+                punchline: `test punchline ${this.counter}`,
+            };
+
+            // 新的cardList数据
+            const newCardList = prevCardList.concat(card);
+
+            // 返回state毒地想
+            return {
+                cardList: newCardList,
+            };
+        };
+        this.setState(callback);
+    }
+    
     // 渲染组件
     render() {
         return (
@@ -42,6 +68,9 @@ export default class PuzzleCardsPage extends React.Component {
                         );
                     })
                 }
+                <div>
+                    <Button onClick={this.addNewCard}> 添加卡片 </Button>
+                </div>
             </div>
         );
     }
